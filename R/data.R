5c68727f9355b99c0cb683a0bcf06637f0b19da1
robinlovelace <rob00x@gmail.com>
2023-09-05 11:23:58 +0100

    Tests passing, now warnings

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Dataset representing the road network in Oldenburg, Germany
+#'
+#' See info in the 'data-raw' folder to see how this dataset was created
+#' @examples
+#' net_oldenburg_raw
+"net_oldenburg_raw"
+
+#' Dataset representing pubs in Oldenburg, Germany
+#'
+#' See info in the 'data-raw' folder to see how this dataset was created
+#' @examples
+#' net_oldenburg_raw
+"points_oldenburg"
