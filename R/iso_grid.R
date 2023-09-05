140de87ee10cc3ac82eb6d6b83e56c35a6de2d1a
robinlovelace <rob00x@gmail.com>
2023-09-05 13:19:08 +0100

    Add h3o, close #1

--- R/iso_grid.R
+++ R/iso_grid.R
@@ -1,11 +1,19 @@
 #' Create hexagonal grid
+#' 
+#' Create a hexagonal grid from an sf object
 #'
 #' @param x An sf object the convex hull of which will determine the hex grid
-#' @param ... Extra arguments passed to the grid generator (TBC)
+#' @param resolution The resolution of the hex grid
+#' @return An sf object
 #' @export
 #' @examples
 #' x = net_oldenburg_raw
 #' iso_grid(x)
-iso_grid = function(x, ...) {
-  sf::st_convex_hull(sf::st_union(x))
+iso_grid = function(x, resolution = 9) {
+  poly = sf::st_convex_hull(sf::st_union(x))
+  hex = h3o::sfc_to_cells(poly, resolution = resolution)
+  hex_flat = h3o::flatten_h3(hex)
+  hex_sfc = sf::st_as_sfc(hex_flat)
+  hex_sfc
 }
+
