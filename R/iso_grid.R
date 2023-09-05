b50baf8a462693ec33146e0b4e6d1081e79430c2
robinlovelace <rob00x@gmail.com>
2023-09-05 14:26:58 +0100

    Add euclidean function, close #3

--- R/iso_grid.R
+++ R/iso_grid.R
@@ -27,6 +27,68 @@ iso_grid = function(x, resolution = 9) {
   hex_sfc
 }
 
-#' Calculate voronoi-style polygons based on network distance/time with sfnetworks
+# # example code for the function, to be packaged:
+
+# ```{r}
+# hex_grid = stplanr::geo_projected(
+#   voronoi,
+#   st_make_grid,
+#   cellsize = 200,
+#   square = FALSE
+# )
+# hex_grid = hex_grid[poly]
+# tm_shape(hex_grid) + tm_polygons() +
+#   tm_shape(points) + tm_dots(col = "red", size = 0.8)
+# ```
+
+# We'll iterate over every hex cell to find the nearest pub, first using nearest distances:
+
+# ```{r}
+# hex_df = data.frame(name = NA)
+# hex_centroids = st_as_sf(st_centroid(hex_grid))
+# nearest_points = st_join(hex_centroids, points, join = nngeo::st_nn, k = 1, progress = FALSE)
+# hex_joined = st_sf(
+#   st_drop_geometry(nearest_points),
+#   geometry = hex_grid
+# )
+# hex_joined_centroids = st_centroid(hex_joined)
+# voronoi_hex = hex_joined |>
+#   group_by(name) |>
+#   summarise(n = n())
+# tm_shape(voronoi_hex, bb = st_bbox(voronoi)) + tm_polygons(col = "name") +
+#   tm_shape(points) + tm_dots(col = "red", size = 0.8) +
+#   tm_shape(voronoi) + tm_borders(col = "blue", lwd = 5) +
+#   tm_layout(legend.outside = TRUE)
+# ```
+
+#' Calculate voronoi-style polygons based on grid with Euclidean distances
 #' 
-#' @param x An sfnetworks object
+#' @note The function groups by the first column in the points object which should be unique (e.g. OSM ID).
+#' 
+#' @param points An sf object of points
+#' @param grid An sf object of a grid
+#' @export
+#' @examples
+#' points = points_oldenburg
+#' x = net_oldenburg_raw
+#' grid = iso_grid(x)
+#' vgrid = iso_vgrid(points, grid)
+iso_vgrid = function(points, grid) {
+  grid_sf = sf::st_as_sf(grid)
+  grid_centroids = sf::st_centroid(grid_sf)
+  nearest_points = sf::st_join(grid_centroids, points, join = nngeo::st_nn, k = 1, progress = FALSE)
+  grid_joined = sf::st_sf(
+    sf::st_drop_geometry(nearest_points),
+    geometry = grid
+  )
+  grid_joined_centroids = sf::st_centroid(grid_joined)
+  voronoi_grid = grid_joined |>
+    dplyr::group_by(dplyr::across(1)) |>
+    dplyr::summarise()
+  voronoi_grid
+}
+
+# #' Calculate voronoi-style polygons based on network distance/time with sfnetworks
+# #' 
+# #' @param x An sfnetworks object
+
