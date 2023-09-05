02c0dbeb5dde1542b11a0c27201711b22c1a4618
Robin Lovelace <Robinlovelace@users.noreply.github.com>
2023-09-05 14:39:18 +0200

    1 implement hex grid function (#2)
    * Add tests for #1
    
    * Add h3o, close #1
    
    * Update R/iso_grid.R
    
    * Update docs, close #1
    
    * Add note, thanks @josiahparry

--- R/iso_grid.R
+++ R/iso_grid.R
@@ -1,11 +1,28 @@
 #' Create hexagonal grid
+#' 
+#' Create a hexagonal grid from an sf object.
+#' 
+#' See the documentation of the upstream
+#' [`h3`](https://h3geo.org/docs/core-library/restable/#average-area-in-km2)
+#' library for more information.
+#' Note: default value of 9 represents around 0.1 km^2, you may way to consider
+#' higher values (e.g. 10, which is around 0.015 km^2 or 15k m^2) for more detailed
+#' analysis or lower values for larger areas.
+#' 
+#' @note This function is a wrapper for `h3o::sfc_to_cells` and `h3o::flatten_h3`.
+#' It assumes that the input object has a geographic (lon/lat) coordinate system, e.g. EPSG:4326.
 #'
 #' @param x An sf object the convex hull of which will determine the hex grid
-#' @param ... Extra arguments passed to the grid generator (TBC)
+#' @param resolution The resolution of the hex grid
+#' @return An sfc object
 #' @export
 #' @examples
 #' x = net_oldenburg_raw
 #' iso_grid(x)
-iso_grid = function(x, ...) {
-  sf::st_convex_hull(sf::st_union(x))
+iso_grid = function(x, resolution = 9) {
+  poly = sf::st_convex_hull(sf::st_union(x))
+  hex = h3o::sfc_to_cells(poly, resolution = resolution)
+  hex_flat = h3o::flatten_h3(hex)
+  hex_sfc = sf::st_as_sfc(hex_flat)
+  hex_sfc
 }
---
+++ data-raw/tests/test-h3o.R
@@ -0,0 +1,26 @@
+# Aim: test the h3o R package
+
+# Starting point: hex grids in sf
+remotes::install_github("isonoi/isonoi")
+remotes::install_github("josiahparry/h3o")
+library(isonoi)
+library(sf)
+poly = sf::st_convex_hull(sf::st_union(net_oldenburg_raw))
+hex_grid = stplanr::geo_projected(
+  sf::st_as_sf(poly),
+  sf::st_make_grid,
+  cellsize = 200,
+  square = FALSE
+)
+hex_grid_smaller = hex_grid[net_oldenburg_raw]
+plot(hex_grid)
+plot(hex_grid_smaller, add = TRUE, col = "red")
+plot(net_oldenburg_raw, add = TRUE, col = "blue")
+
+hex = h3o::sfc_to_cells(poly, resolution = 9)
+hex_flat = h3o::flatten_h3(hex)
+hex_sfc = sf::st_as_sfc(hex_flat)
+plot(hex_grid)
+plot(hex_grid_smaller, add = TRUE, col = "red")
+plot(hex_sfc, add = TRUE, col = "red")
+
