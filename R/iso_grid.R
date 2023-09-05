a0961071e5a72bf2cfada5f184776bf9f85b86f8
robinlovelace <rob00x@gmail.com>
2023-09-05 13:35:18 +0100

    Update docs, close #1

--- R/iso_grid.R
+++ R/iso_grid.R
@@ -1,9 +1,16 @@
 #' Create hexagonal grid
 #' 
-#' Create a hexagonal grid from an sf object
+#' Create a hexagonal grid from an sf object.
+#' 
+#' See the documentation of the upstream
+#' [`h3`](https://h3geo.org/docs/core-library/restable/#average-area-in-km2)
+#' library for more information.
+#' Note: default value of 9 represents around 0.1 km^2, you may way to consider
+#' higher values (e.g. 10, which is around 0.015 km^2 or 15k m^2) for more detailed
+#' analysis or lower values for larger areas.
 #'
 #' @param x An sf object the convex hull of which will determine the hex grid
-#' @param resolution The resolution of the hex grid
+#' @param resolution The resolution of the hex grid, 9 by default
 #' @return An sf object
 #' @export
 #' @examples
@@ -16,4 +23,3 @@ iso_grid = function(x, resolution = 9) {
   hex_sfc = sf::st_as_sfc(hex_flat)
   hex_sfc
 }
-
